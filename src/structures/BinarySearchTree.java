1288ed3ff077b0ff69622558e230b5b8bb8ae933
Terrance J Peters <terrancejpeters@gmail.com>
2016-04-07 20:47:08 -0400

    Current at 65

---
+++ src/structures/BinarySearchTree.java
@@ -0,0 +1,319 @@
+package structures;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BinarySearchTree<T extends Comparable<T>> implements BSTInterface<T> {
+	protected BSTNode<T> root;
+	private int size;
+
+	public boolean isEmpty() {
+		return root == null;
+	}
+
+	public int size() {
+		return size;
+	}
+
+	public boolean contains(T t) {
+		// TODO
+		if (get(t).equals(t)) {
+			return true;
+		}
+		return false;
+	}
+
+	public boolean remove(T t) {
+		if (t == null) {
+			throw new NullPointerException();
+		}
+		Iterator<T> toTraverse = inorderIterator();
+		while (toTraverse.hasNext()) {
+			T temp = toTraverse.next();
+			if (temp.equals(t)) {
+
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public T get(T t) throws NullPointerException {
+		if (t == null) {
+			throw new NullPointerException();
+		}
+		Iterator<T> toTraverse = inorderIterator();
+		while (toTraverse.hasNext()) {
+			T temp = toTraverse.next();
+			if (temp.equals(t)) {
+				return temp;
+			}
+		}
+		return null;
+	}
+
+	public void add(T t) {
+		root = addToSubtree(t, root);
+		size++;
+	}
+
+	private BSTNode<T> addToSubtree(T t, BSTNode<T> node) {
+		if (node == null) {
+			return new BSTNode<T>(t, null, null);
+		}
+		if (t.compareTo(node.getData()) <= 0) {
+			node.setLeft(addToSubtree(t, node.getLeft()));
+		} else {
+			node.setRight(addToSubtree(t, node.getRight()));
+		}
+		return node;
+	}
+
+	@Override
+	public T getMinimum() {
+		Iterator<T> toGet = preorderIterator();
+		T rval = null;
+		if (toGet.hasNext())
+			rval = toGet.next();
+		return rval;
+	}
+
+	@Override
+	public T getMaximum() {
+		Iterator<T> toGet = preorderIterator();
+		T temp = null;
+		while (toGet.hasNext()) {
+			temp = toGet.next();
+		}
+		return temp;
+	}
+
+	@Override
+	public int height() {
+		return heightHelper(root);
+	}
+
+	public int heightHelper(BSTNode<T> node) {
+		if (node == null) {
+			return -1;
+		}
+		if (node.getLeft() == null & node.getRight() == null) {
+			return 0;
+		}
+		if (node.getRight() == null) {
+			return 1 + heightHelper(node.getLeft());
+		}
+		if (node.getLeft() == null) {
+			return 1 + heightHelper(node.getRight());
+		}
+		int value = node.getLeft().getData().compareTo(node.getRight().getData());
+		if (value > 0) {
+			return 1 + heightHelper(node.getLeft());
+		} else {
+			return 1 + heightHelper(node.getRight());
+		}
+	}
+
+	@Override
+	public Iterator<T> preorderIterator() {
+		Queue<T> queue = new LinkedList<T>();
+		preorderTraverse(queue, root);
+		return queue.iterator();
+	}
+
+	public void preorderTraverse(Queue<T> queue, BSTNode<T> node) {
+		if (node != null) {
+			queue.add(node.getData());
+			preorderTraverse(queue, node.getLeft());
+			preorderTraverse(queue, node.getRight());
+		}
+	}
+
+	@Override
+	public Iterator<T> inorderIterator() {
+		Queue<T> queue = new LinkedList<T>();
+		inorderTraverse(queue, root);
+		return queue.iterator();
+	}
+
+	private void inorderTraverse(Queue<T> queue, BSTNode<T> node) {
+		if (node != null) {
+			inorderTraverse(queue, node.getLeft());
+			queue.add(node.getData());
+			inorderTraverse(queue, node.getRight());
+		}
+	}
+
+	@Override
+	public Iterator<T> postorderIterator() {
+		Queue<T> queue = new LinkedList<T>();
+		postorderTraverse(queue, root);
+		return queue.iterator();
+	}
+
+	public void postorderTraverse(Queue<T> queue, BSTNode<T> node) {
+		if (node != null) {
+			postorderTraverse(queue, node.getLeft());
+			inorderTraverse(queue, node.getRight());
+			queue.add(node.getData());
+		}
+
+	}
+
+	// Need to figure this out
+	@Override
+	public boolean equals(BSTInterface<T> other) {
+		// TODO
+		if (size != other.size()) {
+			return false;
+		}
+		Iterator<T> thisIt = inorderIterator();
+		Iterator<T> otherIt = other.inorderIterator();
+		while (thisIt.hasNext()) {
+			if (!thisIt.next().equals(otherIt.next())) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	@Override
+	public boolean sameValues(BSTInterface<T> other) {
+		// TODO
+		if (equals(other)) {
+			return true;
+		}
+		LinkedList<T> otherVals = new LinkedList<T>();
+		LinkedList<T> thisVals = new LinkedList<T>();
+		Iterator<T> thisIt = inorderIterator();
+		Iterator<T> otherIt = other.inorderIterator();
+		while (thisIt.hasNext()) {
+			thisVals.add(thisIt.next());
+		}
+		while (otherIt.hasNext()) {
+			otherVals.add(otherIt.next());
+		}
+		for (int i = 0; i < thisVals.size(); i++) {
+			T toFind = thisVals.get(i);
+			if (!otherVals.contains(toFind)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	@Override
+	public boolean isBalanced() {
+		// TODO
+		if (size == 0) {
+			return true;
+		}
+		double lowBound = Math.pow(2,height());
+		double highBound = Math.pow(2, height() + 1);
+		if (lowBound <= size && size < highBound){
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public void balance() {
+		/*TreeToVine(root);
+		LinkedList<T> toBalance = getAsLL();
+		BinarySearchTree<T> temp = new BinarySearchTree<T>();
+		*/
+		
+		LinkedList<T> nodes = getAsLL();
+		insertTree(0, nodes.size()-1, nodes);
+	}
+	
+	public void insertTree(int lower, int upper, LinkedList<T> nodes){
+		if (lower == upper)
+			add(nodes.get(lower));
+		else if (lower + 1 == upper){
+			add(nodes.get(lower));
+			add(nodes.get(upper));
+		}
+		else{
+			int mid = (lower + upper)/2;
+			add(nodes.get(mid));
+			insertTree(lower, mid - 1, nodes);
+		}
+	}
+	
+	/*
+	public void TreeToVine(BSTNode<T> root){
+		BSTNode<T> tail = root;
+		BSTNode<T> rest = tail.getRight();
+		while (rest != null){
+			if (rest.getLeft() == null){
+				tail = rest;
+				rest = rest.getRight();
+			}
+			else{
+				BSTNode<T> temp = rest.getLeft();
+				rest.getLeft().setRight(rest.getRight());
+				temp.getRight().setSelf(rest);
+				rest = temp;
+				tail.setRight(temp);
+			}
+		}
+	}*/
+	
+	public LinkedList<T> getAsLL(){
+		LinkedList<T> thisVals = new LinkedList<T>();
+		Iterator<T> thisIt = inorderIterator();
+		while (thisIt.hasNext()) {
+			thisVals.add(thisIt.next());
+		}
+		return thisVals;
+		
+	}
+
+	@Override
+	public BSTNode<T> getRoot() {
+		// DO NOT MODIFY
+		return root;
+	}
+
+	public static <T extends Comparable<T>> String toDotFormat(BSTNode<T> root) {
+		// DO NOT MODIFY
+		// see project description for explanation
+
+		// header
+		int count = 0;
+		String dot = "digraph G { \n";
+		dot += "graph [ordering=\"out\"]; \n";
+		// iterative traversal
+		Queue<BSTNode<T>> queue = new LinkedList<BSTNode<T>>();
+		queue.add(root);
+		BSTNode<T> cursor;
+		while (!queue.isEmpty()) {
+			cursor = queue.remove();
+			if (cursor.getLeft() != null) {
+				// add edge from cursor to left child
+				dot += cursor.getData().toString() + " -> " + cursor.getLeft().getData().toString() + ";\n";
+				queue.add(cursor.getLeft());
+			} else {
+				// add dummy node
+				dot += "node" + count + " [shape=point];\n";
+				dot += cursor.getData().toString() + " -> " + "node" + count + ";\n";
+				count++;
+			}
+			if (cursor.getRight() != null) {
+				// add edge from cursor to right child
+				dot += cursor.getData().toString() + " -> " + cursor.getRight().getData().toString() + ";\n";
+				queue.add(cursor.getRight());
+			} else {
+				// add dummy node
+				dot += "node" + count + " [shape=point];\n";
+				dot += cursor.getData().toString() + " -> " + "node" + count + ";\n";
+				count++;
+			}
+
+		}
+		dot += "};";
+		return dot;
+	}
+}
---
+++ src/structures/ScapegoatTree.java
@@ -0,0 +1,55 @@
+package structures;
+
+public class ScapegoatTree<T extends Comparable<T>> extends
+		BinarySearchTree<T> {
+
+	private int upperBound;
+	
+	/**
+	 * Adds an element to the tree.
+	 * 
+	 * The modified tree must still obey the BST rule, though it might not be
+	 * balanced.
+	 * 
+	 * In addition to obeying the BST rule, the resulting tree must also obey
+	 * the scapegoat rule. 
+	 * 
+	 * This method must only perform rebalancing of subtrees when indicated
+	 * by the scapegoat rule; do not unconditionally call balance() 
+	 * after adding, or you will receive no credit. 
+	 * See the project writeup for details.
+	 * 
+	 * @param element
+	 * @throws NullPointerException if element is null
+	 */
+	@Override
+	public void add(T element) {
+		// TODO		
+	}
+	
+	/**
+	 * Attempts to remove one copy of an element from the tree, returning true
+	 * if and only if such a copy was found and removed.
+	 * 
+	 * The modified tree must still obey the BST rule, though it might not be
+	 * balanced.
+	 * 
+	 * In addition to obeying the BST rule, the resulting tree must also obey
+	 * the scapegoat rule.
+	 * 
+	 * This method must only perform rebalancing of subtrees when indicated
+	 * by the scapegoat rule; do not unconditionally call balance() 
+	 * after removing, or you will receive no credit. 
+	 * See the project writeup for details.
+
+	 * @param element
+	 * @return true if and only if an element removed
+	 * @throws NullPointerException if element is null
+	 */
+	@Override
+	public boolean remove(T element) {
+		// TODO
+		return false;
+	}
+
+}
